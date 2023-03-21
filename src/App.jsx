c66505099ccf2336fea4f3a98992ce022d392724
Shubham Tiwari <shubhamtiwari5100@gmail.com>
2023-03-21 09:12:46 +0530

    upload

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import DateTimePicker from 'react-datetime-picker'
+
+function App() {
+ const [reminderMsg, setReminderMsg] = useState("")
+ const [remindAt, setRemindAt] = useState()
+ const[reminderList,setReminderList] = useState([])
+ useEffect(()=>{
+  axios.get("http://localhost:9000/getAllReminder").then(res => setReminderList(res.data))
+
+ },[])
+
+ const addReminder = () =>{
+  axios.post("http://localhost:9000/addReminder",{reminderMsg, remindAt}).then(res => setReminderList(res.data))
+  }
+
+ 
+
+ const deleteReminder=()=>{
+
+ }
+
+   return (
+    <div className="App">
+      {console.log(reminderList)}
+      <div className="homepage">
+        <div className="homepage_header">
+          <h1>Remind me </h1>
+          <input type="text" placeholder='Reminder notes here ...' value={reminderMsg} onChange={e => setReminderMsg(e.target.value)}/>
+
+          <DateTimePicker
+          value={remindAt}
+          onChange={setRemindAt}
+          minDate={new Date()}
+          minutePlaceholder="mm"
+          hourPlaceholder="hh"
+          dayPlaceholder="DD"
+          monthPlaceholder="MM"
+          yearPlaceholder="YYYY"
+
+          />
+
+          <div className="button" onClick={addReminder}>Add Reminder</div>
+        </div>
+
+        <div className="homepage_body">
+          <div className="reminder_card">
+            <h2>Reminder Note</h2>
+            <h3>Remind me at:</h3>
+            <p>19/03/2023 @7AM</p>
+            <div className="button" onClick={deleteReminder}>Delete</div> 
+          </div>
+
+         
+
+
+        </div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
